8bea1823d7b3e21a0b59408d01e542b8010792fe
juang-10 <63117558+juang-10@users.noreply.github.com>
2021-08-28 12:53:52 -0500

    add year options to the select

--- js/app.js
+++ js/app.js
@@ -1,25 +1,39 @@
 // Variables
-const resultado = document.querySelector('#resultado');
+const marca = document.querySelector('#marca');
 const year = document.querySelector('#year');
+const minimo = document.querySelector('#minimo');
+const maximo = document.querySelector('#maximo');
+const puertas = document.querySelector('#puertas');
+const transmision = document.querySelector('#transmision');
+const color = document.querySelector('#color');
+const resultado = document.querySelector('#resultado');
 const max = new Date().getFullYear();
 const min =  max - 10;
 
-console.log(max);
-console.log(min);
+// Generar objeto con la búsqueda
+const datosBusqueda = {
+    marca: '',
+    year: '',
+    minimo: '',
+    maximo: '',
+    puertas: '',
+    transmision: '',
+    color: '',
+}
 
 // Eventos
 document.addEventListener('DOMContentLoaded', () => {
     mostrarAutos(); //Muestra los comentarios al cargar
-
+    
     // Llena las opciones de años
+    llenarSelect();
 });
-
 
 // Funciones
 function mostrarAutos() {
     autos.forEach(auto => {
 
-        const { marca, modelo, year, puerta, transmision, precio, color  } = auto;
+        const { marca, modelo, year, puertas, transmision, precio, color  } = auto;
         const autoHTML = document.createElement('p');
 
         autoHTML.textContent = `
@@ -34,5 +48,17 @@ function mostrarAutos() {
 
 // Genera los años del select
 function llenarSelect() {
-    
+    for( let i = max; i >= min; i-- ){
+        const opcion =  document.createElement('option');
+        opcion.value = i;
+        opcion.textContent = i;
+        year.appendChild(opcion); // Agrega las opciones de año al select
+    }
 }
+
+// function seleccionarMarca() {
+//     this.marca = document.createElement('option');
+//     option.value = this.marca;
+//     option.textContent = this.marca;
+//     this.marca.appendChild(option);
+// }
